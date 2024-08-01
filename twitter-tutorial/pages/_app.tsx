d21a771bc295fe4bd03db488db594fdf098a8cb1
Collin Fiske <132860072+CollinFiske@users.noreply.github.com>
2024-07-31 20:28:34 -0400

    layout work

--- twitter-tutorial/pages/_app.tsx
+++ twitter-tutorial/pages/_app.tsx
@@ -1,9 +1,12 @@
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  <Layout>
-    return <Component {...pageProps} />;
-  </Layout>
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  )
+  
 }
