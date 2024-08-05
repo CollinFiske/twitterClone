9fe3a3f69c680a35655ea7b120491abcc408b899
Collin Fiske <132860072+CollinFiske@users.noreply.github.com>
2024-08-05 10:37:27 -0400

    added to avatar

--- twitter-tutorial/components/modals/LoginModal.tsx
+++ twitter-tutorial/components/modals/LoginModal.tsx
@@ -3,6 +3,7 @@ import { useCallback, useState } from "react";
 import Input from "../Input";
 import Modal from "../Modal";
 import useRegisterModal from "@/hooks/useRegisterModal";
+import { signIn } from "next-auth/react";
 
 const LoginModal = () => {
 
@@ -27,7 +28,10 @@ const LoginModal = () => {
         try {
             setIsLoading(true);
 
-            // TODO ADD LOG IN
+            await signIn('credentials', {
+                email,
+                password
+            })
 
             loginModal.onClose();
 
@@ -38,7 +42,7 @@ const LoginModal = () => {
             setIsLoading(false);
 
         }
-    }, [loginModal]);
+    }, [loginModal, email, password]);
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
@@ -50,6 +54,7 @@ const LoginModal = () => {
                 />
             <Input
                 placeholder="Password"
+                type="password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
                 disabled={isLoading}
